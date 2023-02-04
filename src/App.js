5762e41f27e7af6810cd167e732411784242dfda
Dakota Greenlee <dg2120032@gmail.com>
2023-02-04 11:03:12 -0800

    gallery

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 // Import components
 import './App.css';
+import Gallery from './Components/Gallery'
 
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
+        <Gallery/>
         {/* Your content will go here! */}
       </div>
     </div>
